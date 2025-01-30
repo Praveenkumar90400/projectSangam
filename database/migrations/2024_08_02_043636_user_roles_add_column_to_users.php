80e2ddc2c7a810c7d7b93976d40af22b72b58b00
Praveen kumar <119965918+Praveenkumar90400@users.noreply.github.com>
2025-01-30 20:40:41 +0530

    Update 2024_08_02_043636_user_roles_add_column_to_users.php

--- database/migrations/2024_08_02_043636_user_roles_add_column_to_users.php
+++ database/migrations/2024_08_02_043636_user_roles_add_column_to_users.php
@@ -11,20 +11,53 @@ return new class extends Migration
      *
      * @return void
      */
-   public function up()
+  use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
 {
-    if (!Schema::hasColumn('users', 'role_id')) {
+    public function up()
+    {
         Schema::table('users', function (Blueprint $table) {
-            $table->bigInteger('role_id')->unsigned()->nullable()->after('email');
-            $table->tinyInteger('status')->default(1);
-            $table->bigInteger('phone')->unsigned()->nullable();
-            $table->string('city')->nullable();
-            $table->string('state')->nullable();
-            $table->integer('pincode')->nullable();
-            $table->string('address')->nullable();
+            if (!Schema::hasColumn('users', 'role_id')) {
+                $table->bigInteger('role_id')->unsigned()->nullable()->after('email');
+            }
+            if (!Schema::hasColumn('users', 'status')) {
+                $table->tinyInteger('status')->default(1);
+            }
+            if (!Schema::hasColumn('users', 'phone')) {
+                $table->bigInteger('phone')->unsigned()->nullable();
+            }
+            if (!Schema::hasColumn('users', 'city')) {
+                $table->string('city')->nullable();
+            }
+            if (!Schema::hasColumn('users', 'state')) {
+                $table->string('state')->nullable();
+            }
+            if (!Schema::hasColumn('users', 'pincode')) {
+                $table->integer('pincode')->nullable();
+            }
+            if (!Schema::hasColumn('users', 'address')) {
+                $table->string('address')->nullable();
+            }
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+            $table->dropColumn('status');
+            $table->dropColumn('phone');
+            $table->dropColumn('city');
+            $table->dropColumn('state');
+            $table->dropColumn('pincode');
+            $table->dropColumn('address');
         });
     }
 }
+    
 
 
     /**
